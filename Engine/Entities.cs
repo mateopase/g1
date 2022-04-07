890dd8216b1248e992b675b0d3a62c2b805f0581
Mateo Paredes Sepulveda <mateopase@gmail.com>
2022-04-07 09:35:51 -0400

    Working on ECS

--- Engine/Entities.cs
+++ Engine/Entities.cs
@@ -4,21 +4,57 @@ namespace Engine.Entities
 {
     public class Entity
     {
-        private int id;
-        private World world;
+        private int _id;
+        private World _world;
 
         public Entity(World world) {
-            this.world = world;
+            this._world = world;
+        }
+    }
+
+    public interface IComponent { }
+
+    public interface ISystem<T> where T : IComponent
+    {
+        void Process();
+    }
+
+    public class VelocityComponent : IComponent
+    {
+
+    }
+
+    public class PositionComponent : IComponent
+    {
+
+    }
+
+    public class InputSystem : ISystem<InputComponent>
+    {
+        public void Process()
+        {
+            foreach (var inputComponent in this.world
+
         }
     }
 
     public class World
     {
-        private IList<Entities
+        private IList<ISystem<IComponent>> systems;
+
+        public World()
+        {
+            this.systems = new List<ISystem<IComponent>>();
+        }
+
+        public void AddSystem(ISystem<IComponent> system)
+        {
+            this.systems.Add(system);
+        }
 
         public Entity CreateEntity()
         {
-
+            return null;
         }
     }
 }
