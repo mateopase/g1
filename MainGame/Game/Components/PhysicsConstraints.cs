955b86150830a44d38b73fc14042136d4c9a2ef7
Mateo Paredes Sepulveda <mateopase@gmail.com>
2022-04-25 21:36:44 -0400

    Working on ball simulation

---
+++ MainGame/Game/Components/PhysicsConstraints.cs
@@ -0,0 +1,10 @@
+using Microsoft.Xna.Framework;
+
+namespace Components
+{
+    public struct PhysicsConstraints
+    {
+        public Vector2 MaxVelocity;
+        public Vector2 Acceleration;
+    }
+}
---
+++ MainGame/Game/Components/PositionComponent.cs
@@ -0,0 +1,10 @@
+using Microsoft.Xna.Framework;
+
+namespace Components
+{
+    public struct PositionComponent
+    {
+        public Vector2 Position;
+        public Vector2 Destination;
+    }
+}
